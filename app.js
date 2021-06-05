2f4b24e45733207cd4bb28f8023a2c432a66bcff
Kevin-Solomon <kevinsolomon404@gmail.com>
2021-06-05 17:44:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const txtInput = document.querySelector(".txt-input");
+const txtOutput = document.querySelector(".txt-output");
+const button = document.querySelector("#btn-translate");
+
+console.log(txtInput.value)
+function errorHandler(error){
+  console.log("error occured",error);
+  alert("Something went wrong woth the server");
+}
+function fetchValue(){
+  console.log(txtInput.value);
+  fetch(`https://api.funtranslations.com/translate/dothraki.json?text=${txtInput.value}`).then(response => response.json()).then(text => txtOutput.innerText = text.contents.translated).catch(errorHandler);
+}
+button.addEventListener("click", fetchValue);
